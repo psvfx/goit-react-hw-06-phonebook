9251f3e6685e0baf424b46795c622a57391334d1
psvfx <psvfxinvest@gmail.com>
2023-09-18 20:39:30 +0300

    Added componetes and styled

---
+++ src/components/ContactsFilter/ContactsFilter.jsx
@@ -0,0 +1,28 @@
+import { FilterInput, LabelSearchContact } from './ContactsFilter.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { foundContacts } from 'redux/store';
+
+export const ContactsFilter = () => {
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
+
+  const onChange = e => {
+    const searchQuery = e.target.value;
+    return dispatch(foundContacts(searchQuery));
+  };
+  return (
+    <>
+      <LabelSearchContact htmlFor="filter">
+        Search contact by name
+      </LabelSearchContact>
+      <FilterInput
+        type="text"
+        placeholder="Name"
+        autoComplete="off"
+        name="filter"
+        value={filter}
+        onChange={onChange}
+      />
+    </>
+  );
+};
